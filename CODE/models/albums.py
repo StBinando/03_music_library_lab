46c93f224f5976d6213204426d57912988be13a2
Stefano Binando <sbinando@gmail.com>
2021-11-09 18:33:50 +0000

    initializes classes artist and album

---
+++ CODE/models/albums.py
@@ -0,0 +1,5 @@
+class Albulm:
+    def __init__(self, id = None, title, genre, artist_id):
+        self.title = title
+        self.genre = genre
+        self.artist_id = artist_id
---
+++ CODE/models/artists.py
@@ -0,0 +1,4 @@
+class Artist:
+    def __init__(self, id = None, name):
+        self.id = id
+        self.name = name
